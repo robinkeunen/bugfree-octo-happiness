0a10fa8b4c6d53d487be1398dfcda5b37dc542fb
Robin Keunen <robin@keunen.net>
2014-01-03 02:27:52 +0100

    read/write with byte[]

--- kvstore/src/tests/Tests.java
+++ kvstore/src/tests/Tests.java
@@ -1,17 +1,27 @@
 package tests;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
+
 import oracle.kv.KVStore;
 import oracle.kv.KVStoreConfig;
 import oracle.kv.KVStoreFactory;
 import oracle.kv.Key;
+import oracle.kv.Value;
 import oracle.kv.ValueVersion;
 import oracle.kv.avro.AvroCatalog;
 import oracle.kv.avro.GenericAvroBinding;
+import project.Item;
 
 public class Tests {
 	private final KVStore store;
@@ -43,27 +53,81 @@ public class Tests {
 	 */
 	public void go() throws IOException {
 		System.out.println("Tests.go...");
-		
-		Schema.Parser parser = new Schema.Parser();
-		parser.parse(new File("/Users/roke/Dropbox/eclipse-ws/abdr/kvstore/src/project/item.avsc"));
-		Schema schema = parser.getTypes().get("project.Item");
-		AvroCatalog catalog = store.getAvroCatalog();
-		GenericAvroBinding binding = catalog.getGenericBinding(schema);
-		
-		GenericRecord item = new GenericData.Record(schema);
-		item.put("intField1", 3);
-		store.put(Key.createKey("key"), binding.toValue(item));
-		
-		ValueVersion vv = store.get(Key.createKey("key"));
-		GenericRecord read;
-		int readField;
-		
-		if (vv != null) {
-			read = binding.toObject(vv.getValue());
-			readField = (Integer) read.get("intField1");
-			System.out.println("read " + readField);
+
+		Item wItem = Item.createRandomItem();
+		System.out.println("wItem: \n" + wItem.toString()); 
+		byte[] yourBytes;
+
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		ObjectOutput out = null;
+		try {
+			out = new ObjectOutputStream(bos);   
+			out.writeObject(wItem);
+			yourBytes = bos.toByteArray();
+		} finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+			} catch (IOException ex) {
+				// ignore close exception
+			}
+			try {
+				bos.close();
+			} catch (IOException ex) {
+				// ignore close exception
+			}
 		}
 		
+		store.put(Key.createKey("key"), Value.createValue(yourBytes));
+		ValueVersion vv = store.get(Key.createKey("key"));
+
+		ByteArrayInputStream bis = new ByteArrayInputStream(vv.getValue().getValue());
+		ObjectInput in = null;
+		try {
+		  in = new ObjectInputStream(bis);
+		  Item o = (Item) in.readObject(); 
+		  System.out.println(o.toString());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+		  try {
+		    bis.close();
+		  } catch (IOException ex) {
+		    // ignore close exception
+		  }
+		  try {
+		    if (in != null) {
+		      in.close();
+		    }
+		  } catch (IOException ex) {
+		    // ignore close exception
+		  }
+		}
+
+
+
+		//		Schema.Parser parser = new Schema.Parser();
+		//		parser.parse(new File("/Users/roke/Dropbox/eclipse-ws/abdr/kvstore/src/project/item.avsc"));
+		//		Schema schema = parser.getTypes().get("project.Item");
+		//		AvroCatalog catalog = store.getAvroCatalog();
+		//		GenericAvroBinding binding = catalog.getGenericBinding(schema);
+		//		
+		//		GenericRecord item = new GenericData.Record(schema);
+		//		item.put("intField1", 3);
+		//		store.put(Key.createKey("key"), binding.toValue(item));
+		//		
+		//		ValueVersion vv = store.get(Key.createKey("key"));
+		//		GenericRecord read;
+		//		int readField;
+		//		
+		//		if (vv != null) {
+		//			read = binding.toObject(vv.getValue());
+		//			readField = (Integer) read.get("intField1");
+		//			System.out.println("read " + readField);
+		//		}
+
 		System.out.println("Tests.go() ... done");	
 	}
 
