a35857bbe8725784a5f0897b06d69f2fa08e1884
Robin Keunen <robin@keunen.net>
2014-01-04 23:17:32 +0100

    store monitor

--- kvstore/src/project/ConfigsServer.java
+++ kvstore/src/project/ConfigsServer.java
@@ -9,7 +9,10 @@ import oracle.kv.KVStore;
 public class ConfigsServer {
 	private final static List<ServerParameters> lstServer = Arrays.asList(
 				new ServerParameters("store1", "127.0.0.1", "5000"),
-				new ServerParameters("store2", "127.0.0.1", "5020")
+				new ServerParameters("store2", "127.0.0.1", "5020"),
+				new ServerParameters("store3", "127.0.0.1", "5030"),
+				new ServerParameters("store4", "127.0.0.1", "5040"),
+				new ServerParameters("store5", "127.0.0.1", "5050")
 			);
 	
 	public static List<KVStore> getServersStores() {
--- kvstore/src/tests/LoadBalancingTest.java
+++ kvstore/src/tests/LoadBalancingTest.java
@@ -1,19 +1,74 @@
 package tests;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 import project.ConfigsServer;
+import project.application.ClientApplication;
+import project.application.ClientApplicationResult;
 import project.master.StoreMaster;
 
 public class LoadBalancingTest {
 	public static void main(String[] args) {
 		try {
 			LoadBalancingTest t = new LoadBalancingTest();
+			t.go();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public LoadBalancingTest() {
 		StoreMaster.setKVStores(ConfigsServer.getServersStores());
+	}
+
+	private void go() {
+		System.out.println("Load Balancing Test go ...");
+
+		final int clientNumber = 10; 
+
+		// gives the allowed profile targets to the client application (0 to 4)
+		ArrayList<Long> profileTargets = new ArrayList<Long>();
+		for (long pt = 0; pt < 30; pt++)
+			profileTargets.add(pt);
+
+		ExecutorService executor = Executors.newFixedThreadPool(clientNumber);
+
+		// List of Futures to catch results
+		List<Future<ClientApplicationResult>> results = new ArrayList<Future<ClientApplicationResult>>();
+		for (int i = 0; i < clientNumber; i++) {
+
+			// Create and launch callables
+			Callable<ClientApplicationResult> app = new ClientApplication(i, profileTargets);
+			Future<ClientApplicationResult> submit = executor.submit(app);
+			results.add(submit);
+		}
+
+		long sum = 0;
+		// now retrieve the result
+		for (Future<ClientApplicationResult> future : results) {
+			try {
+				sum += future.get().getAverageTransactionTime();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+
+		long average = sum/results.size();
+		System.out.println("  " + clientNumber + " clients");
+		System.out.println("Average transaction execution time:\n    " + average/1000000L + " ms");
+
+		// compute and store new average
+
+
+		System.out.println("Load Balancing Test ... Done");
 	}
 
 }
