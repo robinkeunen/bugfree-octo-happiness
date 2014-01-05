572bcf2b906e19c2e08d69e993fe1ae064f55776
clem <clement.bar@gmail.com>
2014-01-05 16:24:12 +0100

    Supervisor : stop monitors and test server destination after move.

--- kvstore/src/project/master/StoreSupervisor.java
+++ kvstore/src/project/master/StoreSupervisor.java
@@ -25,39 +25,33 @@ public class StoreSupervisor implements Runnable {
 		while(isKeepRunning()) {
 			System.out.println("StoreSupervisor - Start");
 			List<StoreController> lstOver = getStoreController(State.OVERLOADED);
-			if(lstOver.size() > 0) {
-				List<StoreController> lstNotOver = new ArrayList<StoreController>(getStoreController(State.OVERLOADED));
+			// Check all overloaded store.
+			for(StoreController store : lstOver) {
+				List<StoreController> lstNotOver = new ArrayList<StoreController>(getStoreController(State.UNDERLOADED));
 				lstNotOver.addAll(getStoreController(State.LOADED));
-				// Check all overloaded store.
-				for(StoreController store : lstOver) {
-					// If there are no overloaded store.
-					if(!lstNotOver.isEmpty()) {
-						// Store to move profile.
-						StoreController store_targ = lstNotOver.get(0);
-						// Get the bigger profile.
-						Long profilID = store.getMonitor().getProfilMaxItems();
-						if(profilID != null){
-							// Move profile.
-							try {
-								System.out.println("StoreSupervisor - Profil to move : P"+profilID);
-								StoreMaster.getStoreMaster().moveProfil(store, store_targ, profilID);
-								System.out.println("StoreSupervisor - Profil moved");
-								// TODO : Update frequentlyAccessed.
-							} catch (MissingConfigurationException e) {
+				// If there are no overloaded store.
+				if(!lstNotOver.isEmpty()) {
+					// Store to move profile.
+					StoreController store_targ = lstNotOver.get(0);
+					// Get the bigger profile.
+					Long profilID = store.getMonitor().getProfilMaxItems();
+					if(profilID != null){
+						// Move profile.
+						try {
+							System.out.println("StoreSupervisor - Profil to move : P"+profilID);
+							StoreMaster.getStoreMaster().moveProfil(store, store_targ, profilID);
+							System.out.println("StoreSupervisor - Profil moved");
+							// TODO : Update frequentlyAccessed.
+						} catch (MissingConfigurationException e) {
 
-							}
-							// Change store destination if current is overloaded.
-							if(store_targ.getState()==State.OVERLOADED) {
-								lstNotOver.remove(store_targ);
-							}	
-						}
-						else
-							System.out.println("StoreSupervisor - No Profil selected");
+						}	
 					}
-					else {
-						System.out.println("StoreSupervisor - A Store is overloaded but no store available to move profiles.");
-						break;
-					}
+					else
+						System.out.println("StoreSupervisor - No Profil selected");
+				}
+				else {
+					System.out.println("StoreSupervisor - A Store is overloaded but no store available to move profiles.");
+					break;
 				}				
 			}
 			System.out.println("StoreSupervisor - Sleep");
